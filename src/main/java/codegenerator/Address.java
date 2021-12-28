f2d946fb3d0497335cb65fdaeb71fb4d10c9ee79
Ainaz99 <ainaz.eftekhar@gmail.com>
2021-12-29 00:03:48 +0100

    EncapsulateFields refactoring

--- src/main/java/codegenerator/Address.java
+++ src/main/java/codegenerator/Address.java
@@ -4,9 +4,9 @@ package codegenerator;
  * Created by mohammad hosein on 6/28/2015.
  */
 public class Address {
-    public int num;
-    public TypeAddress Type;
-    public varType varType;
+    private int num;
+    private TypeAddress Type;
+    private varType varType;
 
     public Address(int num,varType varType, TypeAddress Type) {
         this.num = num;
@@ -28,5 +28,17 @@ public class Address {
             return "#"+num;
         else
             return num+"";
+    }
+
+    public int getNum(){
+        return num;
+    }
+
+    public TypeAddress getType(){
+        return Type;
+    }
+
+    public varType getVarType(){
+        return varType;
     }
 }
